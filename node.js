d83f25247345dfb8ac460bba7cdb9e5bf3f57197
Adrian T <43932476+Ssmidge@users.noreply.github.com>
2020-12-23 12:15:53 -0500

    Add files via upload

---
+++ node.js
@@ -0,0 +1,22 @@
+const { exec } = require("child_process")
+const promptly = require('promptly');
+
+
+const validator = function (value) {
+    if (value.length < 1) {
+        throw new Error('Minimum length is 1');
+    }
+
+    return value;
+};
+
+(async (value) => {
+    const language = await promptly.prompt('Language: ', { validator });
+
+    const input = language.toLowerCase()
+    if (input == "nodejs") {
+        exec("npm init -y")
+        exec('npm set init.author.name "ChangeMe"')
+    }
+
+})();
